71e26c4c0a55a9074b4d4a8aba72929535f20f37
HuongMT98 <vndong998@gmail.com>
2025-03-14 13:23:16 +0700

    clean setup code

--- Server/server.js
+++ Server/server.js
@@ -7,31 +7,34 @@ import bodyParser from "body-parser"
 // Load environment variables from .env file
 dotenv.config()
 
+// Create an Express server
 const app = express()
-const PORT = process.env.PORT || 5000
-// const URL_DB = process.env.URL_DB
-const URL_DB =
-  "mongodb+srv://vndong998:QLOR4MpPiKVpGgiN@learnnodejsdb.vie8f.mongodb.net/?retryWrites=true&w=majority&appName=LearnNodejsDB"
 
+// Define the port to listen on
+const PORT = process.env.PORT || 5000
+
+// Replace with your own MongoDB connection string from the environment file
+const URL_DB = process.env.URL_DB
+
+// Start the server use Mongoose to connect to the MongoDB database and listen on the specified port
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URL_DB)
+    console.log("1:⭕ DataBase is connected")
+    app.listen(PORT, () =>
+      console.log(`2:⭕ Server running on port 🔺http://localhost:${PORT}🔻`)
+    )
+  } catch (error) {
+    console.log("3:❌ Error connecting to DataBase, try again", error)
+    process.exit(1)
+  }
+}
+connectDB()
+
+// Middleware
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: "20mb" }))
 
-// Start the server use Mongoose to connect to the MongoDB database
-
-mongoose
-  .connect(URL_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server running on port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => console.log(error.message))
-
-app.get("/", (req, res) => {
-  res.send("Hello World")
-})
+// Routes
